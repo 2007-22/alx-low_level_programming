45c594e4a8bae1fe0e82396f60d8c38df93db0f0
2007-22Lonsosou <kozonc@yahoo.com>
2022-12-13 18:33:53 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,17 +17,17 @@ n = rand() -RAND_MAX / 2;
 if (n > 0)
 {
  /* if else if condition is true */
- printf("%d is positive\n", n);
+ printf("%d is positive\n");
  }
 else if (n == 0)
 {
 /* if else if condition is true */
-printf("%d is zero\n", n);
+printf("%d is zero\n");
 }
 else if (n < 0)
 {
 /* if else if condition is true */
-printf("%d is negative\n", n);
+printf("%d is negative\n");
 }
 return (0);
 }
