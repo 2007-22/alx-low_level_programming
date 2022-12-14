61f2060b80038a7dea80a5628239df928f7b150b
2007-22Lonsosou <kozonc@yahoo.com>
2022-12-14 00:57:18 +0000

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,3 +6,29 @@
 */
 int main(void)
 { 
+int ones = '0';
+int tens = '0';
+int hundreds = '0';
+for (hundreds = '0'; hundreds <= '9'; hundreds++)
+{
+for (tens = '0'; tens <= '9'; tens++)/*print tens digits*/
+{
+for (ones = '0'; ones <= '9'; ones++)/*print ones digits*/
+{
+if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+{
+putchar(hundreds);
+putchar(tens);
+putchar(ones);
+if (!(ones == '9' && hundreds == '7' && tens == '8'))/*add commas and spaces*/
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+putchar (0);
+} 
