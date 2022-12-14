8acef3eb34b9d81590055d4f5c8e501149664ba4
2007-22Lonsosou <kozonc@yahoo.com>
2022-12-14 00:42:54 +0000

    task 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,12 +9,11 @@
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is", n);
-    /*n -n % 10;*/
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is", n);
+/*n -n % 10;*/
 if (n > 5)
 {
 printf("%d and is greater than 5\n", n);
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{ 
