f2aa999cfa9c5ee07e45f7a08865a03f776ee7b7
2007-22Lonsosou <kozonc@yahoo.com>
2022-12-15 13:32:15 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,28 +7,14 @@
 int main(void)
  {
 {
-putchar(',');
-{
+putchar('_');
 putchar('p');
-{
 putchar('u');
-{
 putchar('t');
-{
 putchar('c');
-{
 putchar('h');
-{
 putchar('a');
-{
 putchar('r');
-}
-}
-}
-}
-}
-}
-}
 }
 putchar('\n');
 return (0);
